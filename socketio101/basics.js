22ebd2432383b77f0a4280c259e87a684840fff3
Vinh Peters <vinhpeters@outlook.com>
2023-05-11 13:25:28 -0400

    Add simple chat app

---
+++ socketio101/basics.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const app = express()
+const socketio = require('socket.io')
+
+app.use(express.static(__dirname + '/public'))
+
+const expressServer = app.listen(8000)
+const io = socketio(expressServer)
+
+io.on('connect', (socket) => {
+    console.log(socket.id, 'has connected')
+    
+    
+    socket.emit('messageFromServer', { data: 'Welcome to the socket.io server' })
+    socket.on('messageFromClient', (data) => {
+        console.log('Data', data);
+    })
+})
